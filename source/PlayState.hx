e72dc90fbe0cd96f14dcb4c2461c5ee1435fb6b3
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-02-11 04:17:53 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -516,15 +516,6 @@ class PlayState extends MusicBeatState
 
 	override public function create()
 	{
-		if (boyfriend.curCharacter == 'bf' && FlxG.random.bool(1.4096))
-	        {
-	             boyfriend.curCharacter == 'bf-shiny';
-	        }
-		else
-	        {	
-	             boyfriend.curCharacter == 'bf';
-		}
-
 		Paths.clearStoredMemory();
 
 		var compactCombo:String = formatCompactNumber(combo);
