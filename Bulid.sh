599972fee870c58f5bd5a7811a08b437fbf48650
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-08-12 11:40:25 -0400

    Update Bulid.sh

--- Bulid.sh
+++ Bulid.sh
@@ -17,7 +17,7 @@ haxelib remove flixel-tools
 haxelib remove flixel-ui
 haxelib remove flixel
 haxelib install flixel-tools
-haxelib install flixel-ui 2.6.4
+haxelib install flixel-ui
 haxelib install flixel-addons 2.9.0
 haxelib install flixel 4.11.0
 haxelib install tjson
